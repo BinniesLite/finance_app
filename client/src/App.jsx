99de6de7f612097a41d4e3c658485b7b35820474
tranguv <89886131+tranguv@users.noreply.github.com>
2023-05-09 22:52:59 -0400

    Sign Up Page first commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,14 @@
 import { useState } from 'react'
-
+import SignUpPage  from './components/pages/SignUpPage'
 import './App.css'
+// import "node_modules/react-icons";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-       
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-        </button> 
-
-        <p>
-          Hello World
-        </p>
-      </div>
-      <p className="read-the-docs">
-      </p>
+    <div> 
+      <SignUpPage/>
+    </div>
     </>
   )
 }
