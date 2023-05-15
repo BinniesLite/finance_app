d7235c3a4ac52cc5f07d3fbdeed62834e17a6b7a
Trong Hieu Vuong <hieuvg02@gmail.com>
2023-05-15 15:25:16 -0700

    Link login with signup page successul.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,24 @@
 import { useState } from 'react'
 import SignUpPage  from './components/pages/SignUpPage'
 import SignInPage  from './components/pages/SignInPage'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css'
 // import "node_modules/react-icons";
 
 function App() {
   return (
     <>
-    <div> 
-      <SignInPage/>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<SignInPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+        </Routes>
+      </Router>
     </div>
+    {/* <div> 
+      <SignInPage/>
+    </div> */}
     </>
   )
 }
