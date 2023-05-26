132b809a499923262ba8d1d7ff2ca62888efe139
Trong Hieu Vuong <hieuvg02@gmail.com>
2023-05-25 21:57:51 -0700

    Initialize the card view.

--- client/src/components/pages/TransactionsPage.jsx
+++ client/src/components/pages/TransactionsPage.jsx
@@ -6,16 +6,46 @@ import { React, useState } from "react"
 import Divider from '@mui/material/Divider'
 import Root from '@mui/material/Divider'
 import "../../styles/Transactions.css"
-import { Button, TextField } from "@mui/material";
+import { Button, Card, TextField, CardActions, CardActionArea, CardMedia, CardContent, Typography } from "@mui/material";
 
 const TransactionPage = () => {
     return (
         <>
-            <header>Transactions</header>
             <body>
                 <div>
-                        <Divider>CENTER</Divider>
-                        <Divider textAlign="left">LEFT</Divider>
+                        <Divider>
+                            {
+                                <h1>Wallets</h1>
+                            }
+                        </Divider>
+                        <Divider textAlign="left">
+                            {
+                                <Card sx={{ maxWidth: 345 }}>
+                                <CardActionArea>
+                                  <CardMedia
+                                    component="img"
+                                    height="140"
+                                    image="../assets/IMG_5187.PNG"
+                                    alt="green iguana"
+                                  />
+                                  <CardContent>
+                                    <Typography gutterBottom variant="h5" component="div">
+                                      Lizard
+                                    </Typography>
+                                    <Typography variant="body2" color="text.secondary">
+                                      Lizards are a widespread group of squamate reptiles, with over 6,000
+                                      species, ranging across all continents except Antarctica
+                                    </Typography>
+                                  </CardContent>
+                                </CardActionArea>
+                                <CardActions>
+                                  <Button size="small" color="primary">
+                                    Share
+                                  </Button>
+                                </CardActions>
+                              </Card>
+                            }
+                        </Divider>
                         <Divider textAlign="right">RIGHT</Divider>
                 </div>
             </body>
