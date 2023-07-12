99c494b5a328675f5a5674109b34d972d5032a3a
tranguv <89886131+tranguv@users.noreply.github.com>
2023-07-12 14:46:38 -0400

    Add charts

--- client/src/components/pages/dashboard/Dashboard.jsx
+++ client/src/components/pages/dashboard/Dashboard.jsx
@@ -1,12 +1,119 @@
-import React from 'react'
-import Section from '../../Layout/Section/Section'
+import React from 'react';
+import Section from '../../Layout/Section/Section';
+import LineChart from './component/LineChart/LineChart';
+import PieCharts from './component/PieChart/PieCharts';
+import DBCard from './component/DBCard/DBCard';
 
 const Dashboard = () => {
-  return (
-    <Section title="Dashboard">
-      
-    </Section>
-  )
-}
+  //hardcoded data for the charts
+  const lineData = [
+    {
+      name: 'January',
+      expense: 4000,
+      income: 2400,
+      amt: 2400,
+    },
+    {
+      name: 'February',
+      expense: 3000,
+      income: 1398,
+      amt: 2210,
+    },
+    {
+      name: 'March',
+      expense: 2000,
+      income: 9800,
+      amt: 2290,
+    },
+    {
+      name: 'April',
+      expense: 2780,
+      income: 3908,
+      amt: 2000,
+    },
+    {
+      name: 'May',
+      expense: 1890,
+      income: 4800,
+      amt: 2181,
+    },
+    {
+      name: 'June',
+      expense: 2390,
+      income: 3800,
+      amt: 2500,
+    },
+    {
+      name: 'July',
+      expense: 3490,
+      income: 4300,
+      amt: 2100,
+    },
+    {
+      name: 'August',
+      expense: 3490,
+      income: 4300,
+      amt: 2100,
+    },
+    {
+      name: 'September',
+      expense: 3490,
+      income: 4300,
+      amt: 2100,
+    },
+    {
+      name: 'October',
+      expense: 3490,
+      income: 4300,
+      amt: 2100,
+    },
+    {
+      name: 'November',
+      expense: null,
+      income: null,
+      amt: 0,
+    },
+    {
+      name: 'December',
+      expense: null,
+      income: null,
+      amt: 2100,
+    },
+  ];
 
-export default Dashboard
+  const pieData = [
+    {
+      name: 'Group A',
+      value: 2400,
+    },
+    {
+      name: 'Group B',
+      value: 4567,
+    },
+    {
+      name: 'Group C',
+      value: 1398,
+    },
+    {
+      name: 'Group D',
+      value: 9800,
+    },
+    {
+      name: 'Group E',
+      value: 3908,
+    },
+    {
+      name: 'Group F',
+      value: 4800,
+    },
+  ];
+  return (
+    <Section title='Dashboard'>
+      <LineChart data={lineData} />
+      <PieCharts pieData={pieData} />
+      <DBCard difference={3000} positive={true} sx={{ width: '300px', height: '300px'}} value='23444' />
+    </Section>
+  );
+};
+
+export default Dashboard;
---
+++ client/src/components/pages/dashboard/component/DBCard/DBCard.jsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { AiOutlineArrowDown } from 'react-icons/ai';
+import { AiOutlineArrowUp } from 'react-icons/ai';
+import { HiCurrencyDollar } from 'react-icons/hi';
+import {
+  Avatar,
+  Card,
+  CardContent,
+  Stack,
+  SvgIcon,
+  Typography,
+} from '@mui/material';
+
+const DBCard = ({ difference, positive = false, sx, value }) => {
+
+  return (
+    <Card sx={sx}>
+      <CardContent>
+        <Stack
+          alignItems='flex-start'
+          direction='row'
+          justifyContent='space-between'
+          spacing={3}
+        >
+          <Stack spacing={1}>
+            <Typography color='text.secondary' variant='overline'>
+              Budget
+            </Typography>
+            <Typography variant='h4'>${value}</Typography>
+          </Stack>
+          <Avatar
+            sx={{
+              backgroundColor: 'error.main',
+              height: 56,
+              width: 56,
+            }}
+          >
+            <SvgIcon>
+              <HiCurrencyDollar />
+            </SvgIcon>
+          </Avatar>
+        </Stack>
+        {difference && (
+          <Stack alignItems='center' direction='row' spacing={2} sx={{ mt: 2 }}>
+            <Stack alignItems='center' direction='row' spacing={0.5}>
+              <SvgIcon color={positive ? 'success' : 'error'} fontSize='small'>
+                {positive ? <AiOutlineArrowUp /> : <AiOutlineArrowDown />}
+              </SvgIcon>
+              <Typography
+                color={positive ? 'success.main' : 'error.main'}
+                variant='body2'
+              >
+                {difference}%
+              </Typography>
+            </Stack>
+            <Typography color='text.secondary' variant='caption'>
+              Since last week
+            </Typography>
+          </Stack>
+        )}
+      </CardContent>
+    </Card>
+  );
+};
+
+DBCard.prototypes = {
+  difference: PropTypes.number,
+  positive: PropTypes.bool,
+  sx: PropTypes.object,
+  value: PropTypes.string.isRequired,
+};
+
+export default DBCard;
