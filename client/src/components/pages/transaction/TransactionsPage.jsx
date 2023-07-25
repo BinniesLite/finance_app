bb974bb4c866dd9b3572788ae2420ad710c2849c
tranguv <89886131+tranguv@users.noreply.github.com>
2023-07-25 13:55:14 -0400

    pull dev

--- client/src/components/pages/transaction/TransactionsPage.jsx
+++ client/src/components/pages/transaction/TransactionsPage.jsx
@@ -1,32 +1,126 @@
-import { useContext, useState, useEffect, useCallback } from "react";
-import "./Transactions.css";
-import { Stack } from "@mui/material";
-import CustomTabs from "@/components/general/CustomTabs";
+// import { useContext, useState, useEffect, useCallback } from 'react';
+// import './Transactions.css';
+// import { Stack } from '@mui/material';
+// import CustomTabs from '@/components/general/CustomTabs';
+
+// import Section from '../../Layout/Section/Section';
+// import TransactionGridView from './components/TransactionGridView/TransactionGridView';
+// import CustomTable from '../../general/table/CustomTable';
+// import {
+//   formatTransactionList,
+//   generateFakeTransactionData,
+//   generateFakeWallets,
+//   pushTransactions,
+//   pushWallets,
+// } from '@/utils/helper';
+// // import { getTransactions } from "@/utils/http-request";
+// import AppContext from '@/context/app/context';
+
+// const TransactionPage = () => {
+//   const [activeTab, setView] = useState(0);
+//   const [transactionData, setTransactionData] = useState([]);
+
+//   const { transactions, getTransactions, loading } = useContext(AppContext);
+//   const changeView = (event, newView) => {
+//     setView(newView);
+//   };
+
+//   useEffect(() => {
+//     const feedData = async () => {
+//       const wallets = generateFakeWallets(1);
+//       try {
+//         await pushWallets(wallets);
+//       } catch (error) {
+//         console.log(error);
+//       }
+
+//       try {
+//         const trans = await generateFakeTransactionData(1);
+//         console.log(trans);
+//         await pushTransactions(trans);
+//       } catch (error) {
+//         console.log(error);
+//       }
+//     };
+//     const fetchTransactions = async () => {
+//       try {
+//         await getTransactions();
+//         // memoize the function
+//         const formattedTransaction = await formatTransactionList(transactions);
+
+//         setTransactionData(formattedTransaction);
+//       } catch (error) {
+//         console.log(error);
+//       }
+//     };
+//     if (transactions.length === 0 || transactionData.length === 0) {
+//       // feedData();
+//       fetchTransactions();
+//     }
+//   }, [transactionData]);
+
+//   const tabs = [
+//     {
+//       id: 'list',
+//       label: 'List',
+//       component: <CustomTable transactions={transactions} loading={loading} />,
+//     },
+//     {
+//       id: 'grid',
+//       label: 'Grid',
+//       component: <TransactionGridView transactions={transactions} />,
+//     },
+//   ];
+//   return (
+//     <div>
+//       <Section title={'Transaction'}>
+//         <Stack
+//           direction={{ xs: 'column', md: 'row', width: '100%' }}
+//           columnGap={3}
+//         >
+//           <Stack flexDirection='column' width='100%' ml={2}>
+//             <CustomTabs
+//               tabs={tabs}
+//               activeTab={activeTab}
+//               handleChangeTab={changeView}
+//             />
+//           </Stack>
+//         </Stack>
+//         {tabs[activeTab].component}
+//       </Section>
+//     </div>
+//   );
+// };
+
+// export default TransactionPage;
+
+import { useContext, useState, useEffect, useCallback } from 'react';
+import './Transactions.css';
+import { Stack } from '@mui/material';
+import CustomTabs from '@/components/general/CustomTabs';
 
 import Section from '../../Layout/Section/Section';
 import TransactionGridView from './components/TransactionGridView/TransactionGridView';
-import CustomTable from '../../general/table/CustomTable';
 import {
   formatTransactionList,
   generateFakeTransactionData,
   generateFakeWallets,
   pushTransactions,
   pushWallets,
-} from "@/utils/helper";
+} from '@/utils/helper';
 // import { getTransactions } from "@/utils/http-request";
-import AppContext from "@/context/app/context";
-import CustomTable from "../../general/table/CustomTable";
+import AppContext from '@/context/app/context';
+import CustomTable from '../../general/table/CustomTable';
 
 const TransactionPage = () => {
   const [activeTab, setView] = useState(0);
   const [transactionData, setTransactionData] = useState([]);
-  
-  const {transactions, getTransactions, loading} = useContext(AppContext);
+
+  const { transactions, getTransactions, loading } = useContext(AppContext);
   const changeView = (event, newView) => {
     setView(newView);
   };
-  
-  
+
   useEffect(() => {
     const feedData = async () => {
       const wallets = generateFakeWallets(1);
@@ -49,9 +143,8 @@ const TransactionPage = () => {
         await getTransactions();
         // memoize the function
         const formattedTransaction = await formatTransactionList(transactions);
-        
+
         setTransactionData(formattedTransaction);
-        
       } catch (error) {
         console.log(error);
       }
@@ -60,20 +153,17 @@ const TransactionPage = () => {
       // feedData();
       fetchTransactions();
     }
-    
-
   }, [transactionData]);
-  
 
   const tabs = [
     {
-      id: "list",
-      label: "List",
+      id: 'list',
+      label: 'List',
       component: <CustomTable transactions={transactions} loading={loading} />,
     },
     {
-      id: "grid",
-      label: "Grid",
+      id: 'grid',
+      label: 'Grid',
       component: <TransactionGridView transactions={transactions} />,
     },
   ];
