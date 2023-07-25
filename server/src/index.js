0578711b3e45b1b3a1fe29b75b884d9a44cb4ba2
BinniesLite <89041460+BinniesLite@users.noreply.github.com>
2023-07-24 23:39:31 -0400

    Prepare again

--- server/src/index.js
+++ server/src/index.js
@@ -3,10 +3,16 @@ const express = require('express');
 const routers = require('./router');
 const cors = require('cors');
 const timeout = require('express-timeout');
+const compression = require("compression");
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
+
+
 const { 
     corsOptions, 
     PORT, 
-    TIMEOUT  
+    TIMEOUT,
+    RATE_LIMITER
 } = require('./configs/configs');
 
 require('dotenv').config();
@@ -24,6 +30,16 @@ app.use(express.json());
 // Cors configuration
 app.use(cors());
 
+// Compression
+app.use(compression());
+
+// Helmet
+app.use(helmet());
+
+// Rate limiter
+app.use(rateLimit(RATE_LIMITER));
+
+app.use(express.static("public"))
 // timeout
 app.use(timeout(TIMEOUT));
 
