15b7133d0bc052d385953ce29606df99098c6e5e
BinniesLite <89041460+BinniesLite@users.noreply.github.com>
2023-07-27 22:50:29 -0400

    update chat

--- src/validations/transaction.validation.js
+++ src/validations/transaction.validation.js
@@ -4,7 +4,7 @@ const transactionSchema = z.object({
   amount: z.number().positive(),
   description: z.string().min(1).max(255).optional(),
   type: z.enum(["income", "expense"]),
-  walletId: z.string(),
+  walletId: z.string().optional(),
   createdAt: z.string().optional(),
   image: z.string().optional(),
 });
