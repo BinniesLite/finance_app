95c5b8ffc6e54021302dd8f6f340c554278aaae2
BinniesLite <89041460+BinniesLite@users.noreply.github.com>
2023-07-28 19:51:53 -0400

    Something

--- client/src/utils/formatCurrency.js
+++ client/src/utils/formatCurrency.js
@@ -1,11 +1,14 @@
 const formatCurrency = (amount) => {
-  if (typeof amount !== 'number') {
-      return "$0.00";
-  }
-  return amount.toLocaleString('en-US', {
-      style: 'currency',
-      currency: 'USD'
-  });
+  if (amount !== null && amount !== undefined) {
+    return amount.toLocaleString('en-US', {
+        style: 'currency',
+        currency: 'USD'
+    });
+} else {
+    // Handle the case when 'amount' is null or undefined
+    // For example, you can return a default value or an error message
+    return '$0.00';
+}
 }
 
 export default formatCurrency;
